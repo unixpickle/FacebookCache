73235961b882890d6f33be7a75e7fc02b6dc78b7
Alex Nichol <alex@Alexs-MacBook-Pro.local>
2012-06-19 16:07:29 -0400

    Initial Commit

---
+++ FacebookCache/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  FacebookCache
+//
+//  Created by Alex Nichol on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ANAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FacebookCache/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  FacebookCache
+//
+//  Created by Alex Nichol on 6/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
